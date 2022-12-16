9fb047271da52e5bd0070090a2015bad7f6a1d39
Luis Dudamel <ladusa@gmail.com>
2022-12-16 17:21:08 +0100

    Moved main stylesheet to scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
+import "./styles/sass/styles.scss";
+
 const App = (): JSX.Element => {
-  return <div className="App"></div>;
+  return <div className="App">SADSADA</div>;
 };
 
 export default App;
