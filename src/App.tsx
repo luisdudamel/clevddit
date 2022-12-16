eb7b48188a7ac499f38847ff1f649cc8b982cd77
Luis Dudamel <ladusa@gmail.com>
2022-12-16 17:54:21 +0100

    Added color variables on scss file

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import Button from "./components/Button/Button";
 import "./styles/sass/styles.scss";
 
 const App = (): JSX.Element => {
-  return <div className="App">SADSADA</div>;
+  return (
+    <div className="App">
+      <Button text="Log in" />
+    </div>
+  );
 };
 
 export default App;
