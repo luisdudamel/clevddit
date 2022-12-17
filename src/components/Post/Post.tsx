55170596334264203434bd58ee7176d273d40e6b
Luis Dudamel <ladusa@gmail.com>
2022-12-17 17:03:23 +0100

    Added color variables per element. Used map method on components

--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -1,7 +1,11 @@
 import "./Post.scss";
 
 const Post = (): JSX.Element => {
-  return <div className="post-container"></div>;
+  return (
+    <div className="post-container">
+      <h2 className="post-title">sadasdsdasdsad</h2>
+    </div>
+  );
 };
 
 export default Post;
