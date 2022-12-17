8939951ce1206b09cf91295e1ed9382ca09d73d6
Luis Dudamel <ladusa@gmail.com>
2022-12-17 21:58:32 +0100

    Added test for user clicking on post delete icon

--- src/components/Post/Post.test.tsx
+++ src/components/Post/Post.test.tsx
@@ -1,6 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import { mockPost } from "../../mocks/mockPosts";
 import Post from "./Post";
+import userEvent from "@testing-library/user-event";
 
 describe("Given a Post function", () => {
   describe("When it's called with a post with title `ChatGPT is killing it`", () => {
@@ -14,6 +15,23 @@ describe("Given a Post function", () => {
       });
 
       expect(postHeading).toBeInTheDocument();
+    });
+  });
+});
+
+describe("Given a Post function", () => {
+  describe("When its called with a function and the user clicks on the image with the alternative text `Delete icon`", () => {
+    test("Then it should call the received function", async () => {
+      const expectedDeleteText = "Delete icon";
+      const mockDeleteAction = jest.fn();
+
+      render(<Post deleteAction={mockDeleteAction} post={mockPost} />);
+
+      const deleteIcon = screen.getByAltText(expectedDeleteText);
+
+      await userEvent.click(deleteIcon);
+
+      expect(mockDeleteAction).toHaveBeenCalled();
     });
   });
 });
