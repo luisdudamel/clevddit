64f687ea10c760c095e5db872d58908e19fed327
Luis Dudamel <ladusa@gmail.com>
2022-12-17 13:43:26 +0100

    Added homepage layout for header. Tested header logo rendering

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import Header from "../../components/Header/Header";
 import { useAppDispatch } from "../../redux/hooks";
 import { getAllPostsThunk } from "../../redux/thunks/postsThunks";
+import "./HomePage.scss";
 
 const HomePage = (): JSX.Element => {
   const dispatch = useAppDispatch();
@@ -12,7 +13,10 @@ const HomePage = (): JSX.Element => {
 
   return (
     <>
-      <Header />
+      <main className="home-page">
+        <Header />
+        <h1 className="home-page__title">Posts</h1>
+      </main>
     </>
   );
 };
