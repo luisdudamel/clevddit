4a8e8e55359305ffeb0c6aa995ebdadbf3c28008
Luis Dudamel <ladusa@gmail.com>
2022-12-17 17:07:43 +0100

    Added title, author and body to post

--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -2,8 +2,14 @@ import "./Post.scss";
 
 const Post = (): JSX.Element => {
   return (
-    <div className="post-container">
-      <h2 className="post-title">sadasdsdasdsad</h2>
+    <div className="post">
+      <h2 className="post__title">sadasdsdasdsad</h2>
+      <h3 className="post__author">By Elon</h3>
+      <p className="post__body">
+        quia et suscipit nsuscipit recusandae consequuntur expedita et cum
+        nreprehenderit molestiae ut ut quas totam nnostrum rerum est autem sunt
+        rem eveniet architecto
+      </p>
     </div>
   );
 };
