5036047d7831c812bee90ba9b2a8ef11f9e5404f
Luis Dudamel <ladusa@gmail.com>
2022-12-17 22:05:35 +0100

    Tested dispatch call on postlist on delete action

--- src/components/PostList/PostList.test.tsx
+++ src/components/PostList/PostList.test.tsx
@@ -3,6 +3,14 @@ import { Provider } from "react-redux";
 import { mockTwoPosts } from "../../mocks/mockPosts";
 import { store } from "../../redux/store/store";
 import PostList from "./PostList";
+import userEvent from "@testing-library/user-event";
+
+const mockDispatch = jest.fn();
+
+jest.mock("../../redux/hooks", () => ({
+  ...jest.requireActual("../../redux/hooks"),
+  useAppDispatch: () => mockDispatch,
+}));
 
 describe("Given a PostList function", () => {
   describe("When invoked with 2 posts with titles `ChatGPT is killing it` and `Copilot is dying`", () => {
@@ -25,6 +33,23 @@ describe("Given a PostList function", () => {
 
       expect(chatHeading).toBeInTheDocument();
       expect(copilotHeading).toBeInTheDocument();
+    });
+  });
+
+  describe("And the user clicks on one image with the alternative text `Delete icon`", () => {
+    test("Then it should call dispatch", async () => {
+      const expectedAlernativeText = "Delete icon";
+
+      render(
+        <Provider store={store}>
+          <PostList postList={mockTwoPosts} />
+        </Provider>
+      );
+
+      const deleteIcon = screen.getAllByAltText(expectedAlernativeText);
+      await userEvent.click(deleteIcon[0]);
+
+      expect(mockDispatch).toHaveBeenCalled();
     });
   });
 });
