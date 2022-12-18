7961ee3757b015bc431c9a0bf587fea922ac9818
Luis Dudamel <ladusa@gmail.com>
2022-12-18 22:58:24 +0100

    Added form for editing post. Added conditional rendering of form

--- src/pages/PostPage/PostPage.tsx
+++ src/pages/PostPage/PostPage.tsx
@@ -47,17 +47,51 @@ const PostPage = (): JSX.Element => {
       <main className="post-page">
         {loading && <Loader />}
         <div className="post-container">
-          <h1 className="post-page__title">{actualPost.title}</h1>
-          <h2 className="post-page__author">By {actualPost.user?.username}</h2>
-          <div className="post-page__body">
-            <p>{actualPost.body}</p>
-            <img
-              width={40}
-              height={40}
-              src="/img/icons/edit.svg"
-              alt="Edit Icon"
-            />
-          </div>
+          {loading && (
+            <>
+              <h1 className="post-page__title">{actualPost.title}</h1>
+              <h2 className="post-page__author">
+                By {actualPost.user?.username}
+              </h2>
+              <div className="post-page__body">
+                <p>{actualPost.body}</p>
+                <img
+                  width={40}
+                  height={40}
+                  src="/img/icons/edit.svg"
+                  alt="Edit Icon"
+                />
+              </div>{" "}
+            </>
+          )}
+          {!loading && (
+            <>
+              <h1 className="post-page__title--edit">
+                <span className="post-page__title--edit__accent">Edit</span>{" "}
+                {actualPost.title}
+              </h1>
+              <div className="post-page__body--edit">
+                <label className="edit-form__title--label" htmlFor="post-title">
+                  Title
+                </label>
+                <textarea
+                  className="edit-form__title"
+                  name="post-title"
+                  id="post-title"
+                  value={actualPost.title}
+                />
+                <label className="edit-form__body--label" htmlFor="post-body">
+                  Post
+                </label>
+                <textarea
+                  className="edit-form__body"
+                  name="post-body"
+                  id="post-body"
+                  value={actualPost.body}
+                />
+              </div>{" "}
+            </>
+          )}
         </div>
       </main>
     </>
