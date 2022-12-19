2c9d189d41c1586a486b72e99638eb64b47e1706
Luis Dudamel <ladusa@gmail.com>
2022-12-19 11:25:25 +0100

    Tested open post button for calling navigate

--- src/components/Post/Post.test.tsx
+++ src/components/Post/Post.test.tsx
@@ -4,6 +4,13 @@ import Post from "./Post";
 import userEvent from "@testing-library/user-event";
 import { BrowserRouter } from "react-router-dom";
 
+const mockNavigate = jest.fn();
+
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useNavigate: () => mockNavigate,
+}));
+
 describe("Given a Post function", () => {
   describe("When it's called with a post with title `ChatGPT is killing it`", () => {
     test("Then it should render a heading with the text `ChatGPT is killing it`", () => {
@@ -23,7 +30,7 @@ describe("Given a Post function", () => {
     });
   });
 
-  describe("When its called with a function and the user clicks on the image with the alternative text `Delete icon`", () => {
+  describe("When its called with a function and the user clicks on the button with the text `Delete icon`", () => {
     test("Then it should call the received function", async () => {
       const expectedDeleteText = "Delete icon";
       const mockDeleteAction = jest.fn();
@@ -34,11 +41,34 @@ describe("Given a Post function", () => {
         </BrowserRouter>
       );
 
-      const deleteIcon = screen.getByAltText(expectedDeleteText);
+      const deleteIcon = screen.getByRole("button", {
+        name: expectedDeleteText,
+      });
 
       await userEvent.click(deleteIcon);
 
       expect(mockDeleteAction).toHaveBeenCalled();
+    });
+  });
+
+  describe("When user clicks on the button with the text `Open post icon`", () => {
+    test("Then it should call navigate", async () => {
+      const expectedOpenPostText = "Open post icon";
+      const mockDeleteAction = jest.fn();
+
+      render(
+        <BrowserRouter>
+          <Post deleteAction={mockDeleteAction} post={mockPost} />
+        </BrowserRouter>
+      );
+
+      const deleteIcon = screen.getByRole("button", {
+        name: expectedOpenPostText,
+      });
+
+      await userEvent.click(deleteIcon);
+
+      expect(mockNavigate).toHaveBeenCalled();
     });
   });
 });
