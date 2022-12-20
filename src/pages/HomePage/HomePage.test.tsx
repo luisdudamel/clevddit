c45c60fe582bd23bf6c1e4648f5b39158dbe161b
Luis Dudamel <ladusa@gmail.com>
2022-12-20 01:44:34 +0100

    Added test for rendering users on homepage

--- src/pages/HomePage/HomePage.test.tsx
+++ src/pages/HomePage/HomePage.test.tsx
@@ -1,6 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, MemoryRouter } from "react-router-dom";
 import { store } from "../../redux/store/store";
 import HomePage from "./HomePage";
 
@@ -14,6 +14,26 @@ describe("Given a HomePage function", () => {
           <BrowserRouter>
             <HomePage />
           </BrowserRouter>
+        </Provider>
+      );
+
+      const postsHeading = screen.getByRole("heading", {
+        name: expectedHeadingText,
+      });
+
+      expect(postsHeading).toBeInTheDocument();
+    });
+  });
+
+  describe("When it's called and the pathname is `/users`", () => {
+    test("Then it should render a heading with the text `Users`", () => {
+      const expectedHeadingText = "Users";
+
+      render(
+        <Provider store={store}>
+          <MemoryRouter initialEntries={["/users"]}>
+            <HomePage />
+          </MemoryRouter>
         </Provider>
       );
 
